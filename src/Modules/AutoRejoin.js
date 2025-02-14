d17a6da7fed6f82835609a100556a7f35b35a93c
crackbob <crackbob.999@gmail.com>
2025-02-13 20:40:20 -0500

    fix auto rejoin

--- src/Modules/AutoRejoin.js
+++ src/Modules/AutoRejoin.js
@@ -5,10 +5,10 @@ function AutoRejoin () {
         AutoRejoin.interval = undefined;
     } else {
         AutoRejoin.interval = setInterval(function() {
-            if (mainSDKInstance.hooks.frame.document.getElementsByClassName("zm-btn zm-btn-legacy zm-btn--primary")?.[0]?.innerText == "Exit" || document.getElementsByClassName("zm-btn zm-btn-legacy zm-btn--primary zm-btn__outline--blue")?.[0]?.innerText === "Leave") {
+            if (mainSDKInstance.scope.document.getElementsByClassName("zm-btn zm-btn-legacy zm-btn--primary")?.[0]?.innerText == "Exit" || mainSDKInstance.scope.document.getElementsByClassName("zm-btn zm-btn-legacy zm-btn--primary zm-btn__outline--blue")?.[0]?.innerText === "Leave") {
                 localStorage.clear();
                 sessionStorage.clear();
-                mainSDKInstance.hooks.frame.location.reload();
+                mainSDKInstance.scope.location.reload();
             }
         }, 1000);
     }
