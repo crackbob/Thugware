4dd9f58e1ebd8148119eb17f4f594bf167809a37
crackbob <crackbob.999@gmail.com>
2025-04-28 00:43:58 -0400

    fix userscript config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,48 @@
 const path = require("path");
-const { UserscriptPlugin } = require('webpack-userscript');
+const { UserscriptPlugin } = require("webpack-userscript");
 
-module.exports = {
+const commonConfig = {
     mode: "production",
     entry: "./src/main.js",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+};
+
+const userscriptConfig = {
+    ...commonConfig,
     output: {
         path: path.resolve(__dirname, "build"),
-        filename: "Thugware.min.js",
+        filename: "Thugware.user.js",
     },
     plugins: [
         new UserscriptPlugin({
             metajs: false,
             headers: {
-                name: 'Thugware',
-                supportURL: 'https://github.com/crackbob/Thugware',
-                description: 'we do a little thugging',
-                version: '1.0.0',
-                author: 'crackbob',
-                match: '*://app.zoom.us/*',
-                grant: 'none',
-            }
+                name: "Thugware",
+                supportURL: "https://github.com/crackbob/Thugware",
+                description: "we do a little thugging",
+                version: "1.0.0",
+                author: "crackbob",
+                match: "*://app.zoom.us/*",
+                grant: "none",
+            },
         }),
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
 };
+
+const regularScriptConfig = {
+    ...commonConfig,
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "Thugware.min.js",
+    },
+    plugins: [],
+};
+
+module.exports = [userscriptConfig, regularScriptConfig];
